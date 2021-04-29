c743003e8dc0f320945c3e066c0f228d84c78304
Taruna Wahyudi <wahyuditaruna97@gmail.com>
2021-04-29 22:29:48 +0700

    inital project

---
+++ src/scripts/utils/drawer-initiator.js
@@ -0,0 +1,23 @@
+const DrawerInitiator = {
+  init({ button, drawer, content }) {
+    button.addEventListener('click', (event) => {
+      this._toogleDrawer(event, drawer);
+    });
+
+    content.addEventListener('click', (event) => {
+      this._closeDrawer(event, drawer);
+    });
+  },
+
+  _toogleDrawer(event, drawer) {
+    event.stopPropagation();
+    drawer.classList.toggle('open');
+  },
+
+  _closeDrawer(event, drawer) {
+    event.stopPropagation();
+    drawer.classList.remove('open');
+  },
+};
+
+export default DrawerInitiator;
