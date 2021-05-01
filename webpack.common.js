1e7d76cb19bb0c31c7e5d6a3750461c1c5ca6745
Taruna Wahyudi <wahyuditaruna97@gmail.com>
2021-05-01 15:09:17 +0700

    implement indexDB, service worker, and cache API

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 const path = require('path');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 
@@ -38,5 +39,8 @@ module.exports = {
       ],
     }),
     new DotenvWebpackPlugin(),
+    new ServiceWorkerWebpackPlugin({
+      entry: path.resolve(__dirname, 'src/scripts/sw.js'),
+    }),
   ],
 };
